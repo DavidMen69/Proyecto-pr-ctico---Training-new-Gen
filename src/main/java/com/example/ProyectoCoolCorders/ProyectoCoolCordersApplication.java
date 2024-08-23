da7fb1a33eef4a1ebcb7c8ee17be8146f4e6408c
DavidMen69 <davidmenher17@gmail.com>
2024-08-23 15:15:18 -0500

    first commit

---
+++ src/main/java/com/example/ProyectoCoolCorders/ProyectoCoolCordersApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoCoolCorders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCoolCordersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCoolCordersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProyectoCoolCorders/ProyectoCoolCordersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoCoolCorders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCoolCordersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
