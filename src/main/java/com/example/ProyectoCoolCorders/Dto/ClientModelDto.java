aa97e203202443ac03f8810ceb673083ba906b91
Cristian Morales Avendano <cmoralesavendano6@gmail.com>
2024-08-26 12:07:19 -0500

    finish-model-client

--- src/main/java/com/example/ProyectoCoolCorders/Dto/ClientModelDto.java
+++ src/main/java/com/example/ProyectoCoolCorders/Dto/ClientModelDto.java
@@ -1,5 +1,14 @@
 package com.example.ProyectoCoolCorders.Dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ClientModelDto {
     public String document;
     public String name; 
--- src/main/java/com/example/ProyectoCoolCorders/Services/ClientService.java
+++ src/main/java/com/example/ProyectoCoolCorders/Services/ClientService.java
@@ -1,12 +1,16 @@
 package com.example.ProyectoCoolCorders.Services;
 
+import com.example.ProyectoCoolCorders.Dto.ClientModelDto;
 import com.example.ProyectoCoolCorders.Models.ClientModel;
 
 public interface ClientService {
 
-    ClientModel getClientByid(Long id);
-
     ClientModel getClientByDocument(String document);
 
     void createClient(ClientModel document);
+
+    void deleteClient(String  document);
+
+    void updateClient(String document, ClientModelDto clientDTO);
+    
 }
