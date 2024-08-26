c584a702acf6aeb4f34d1fc63f261424118e809d
DavidMen69 <davidmenher17@gmail.com>
2024-08-26 12:07:15 -0500

    Metodos Productos Terminado

---
+++ src/main/java/com/example/ProyectoCoolCorders/Repositories/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.example.ProyectoCoolCorders.Repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.ProyectoCoolCorders.Models.ProductModels;
+
+@Repository
+public interface ProductRepository extends JpaRepository <ProductModels, Long> {
+    //Metodo para obtener El UUID
+    Optional<ProductModels> findByUuid(String uuid);
+
+    //Metodo para verificar si existe un producto con el mismo nombre fantasia
+    boolean existsByFantasyName(String fantasyName);
+
+    
+
+}
---
+++ src/main/java/com/example/ProyectoCoolCorders/Services/Impl/ProducServiceImpl.java
@@ -0,0 +1,109 @@
+package com.example.ProyectoCoolCorders.Services.Impl;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.example.ProyectoCoolCorders.Dto.ProductModelDto;
+import com.example.ProyectoCoolCorders.Exceptions.ProductAlreadyExistsException;
+import com.example.ProyectoCoolCorders.Models.ProductModels;
+import com.example.ProyectoCoolCorders.Repositories.ProductRepository;
+import com.example.ProyectoCoolCorders.Services.ProducService;
+import lombok.RequiredArgsConstructor;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProducServiceImpl implements ProducService{
+    
+    private final ProductRepository productRepository;
+
+    //Metodo Crear Producto
+    @Override
+    public void createProduct(ProductModels product) {
+
+        if(productRepository.existsByFantasyName(product.getFantasyName())){
+            throw new ProductAlreadyExistsException("Producto Con Nombre Fantasia Ya Existe");
+        }
+
+        // Generar UUID
+        product.setUuid(UUID.randomUUID().toString());
+
+        // Convertir El Nombre De Fantasia A Masyusculas
+        product.setFantasyName(product.getFantasyName().toUpperCase());
+        product.setCategory(product.getCategory().toUpperCase());
+
+        // Guardar Producto 
+        productRepository.save(product);
+    }
+
+    // Metodo para Obtener Producto Por UUID
+    @Override
+    public ProductModels getProductByuuid(String uuid) {
+        return productRepository.findByUuid(uuid).orElseThrow(() -> new RuntimeException("Product not found"));
+    }
+
+
+    //Metodo Para Actualizar Producto
+
+    @Override 
+    public boolean updateProduct(String uuid, ProductModelDto productDto){
+        Optional<ProductModels> existingProductOpt = productRepository.findByUuid(uuid);
+
+        if(!existingProductOpt.isPresent()){
+            throw new RuntimeException("Product not found");
+        }
+
+        ProductModels existingProduct = existingProductOpt.get();
+
+        if (!existingProduct.getFantasyName().equalsIgnoreCase(productDto.getFantasyName()) && 
+            productRepository.existsByFantasyName(productDto.getFantasyName().toUpperCase())) {
+            throw new ProductAlreadyExistsException("Producto con nombre fantasía ya existe");
+        }
+
+        boolean isUpdated = false;
+
+        if (!existingProduct.getFantasyName().equalsIgnoreCase(productDto.getFantasyName())) {
+            existingProduct.setFantasyName(productDto.getFantasyName().toUpperCase());
+            isUpdated = true;
+        }
+
+        if (!existingProduct.getCategory().equalsIgnoreCase(productDto.getCategory())) {
+            existingProduct.setCategory(productDto.getCategory().toUpperCase());
+            isUpdated = true;
+        }
+
+        if (!existingProduct.getDescription().equalsIgnoreCase(productDto.getDescription())) {
+            existingProduct.setDescription(productDto.getDescription());
+            isUpdated = true;
+        } 
+
+        if (existingProduct.getPrice() != productDto.getPrice()) {
+            existingProduct.setPrice(productDto.getPrice());
+            isUpdated = true;
+        }
+        if (existingProduct.isAvailable() != productDto.isAvailable()) {
+            existingProduct.setAvailable(productDto.isAvailable());
+            isUpdated = true;
+        }
+
+        if (isUpdated) {
+            productRepository.save(existingProduct);
+        }
+
+        return isUpdated;
+    }
+
+    // Servicio Delete Product By Uuid
+    @Override
+    public boolean deleteProductByUuid(String uuid) {
+        Optional<ProductModels> existingProductOpt = productRepository.findByUuid(uuid);
+
+        if(existingProductOpt.isPresent()){
+            productRepository.delete(existingProductOpt.get());
+            return true;
+        }
+        return false;
+    }
+
+
+}
